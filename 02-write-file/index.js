d17c1fae87959c96864f4965094dbdec20f8763c
dima <zhuikovda.gmail.com>
2022-11-08 17:43:33 +0300

    feat: finish 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const {stdin, stdout} = process;
-const readline = require('readline');
-const filePath = path.join(__dirname, 'text.txt');
-fs.createWriteStream(filePath);
+const filePath = path.join(__dirname, 'file.txt');
+const wrstr = fs.createWriteStream(filePath);
+
 stdout.write('Hello, write text!\n');
 
-  
+stdin.on('data', data => {
+    if (data === 'exit') {        
+        process.exit();
+    }
+    wrstr.write(data);
+});
+
+process.on('SIGINT', () => {
+    process.stdout.write('Good luck!');
+    process.exit();
+});
